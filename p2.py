88a378b9b17e18faefab88ec23ec054eb1c391a8
Dev Aggarwal <devxpy@gmail.com>
2021-01-18 02:39:06 +0530

    fix bugs in p2

--- p2.py
+++ p2.py
@@ -21,16 +21,26 @@ for i in range(0, len(D)):
                 S[j] = "?"
 
         for j in range(num_attrs - 1):
+            to_remove = []
             for k in range(1, len(G)):
-                if G[k][j] != S[j] != "?":
-                    del G[k]
+                if G[k][j] != S[j] and G[k][j] != "?":
+                    to_remove.append(k)
+            for k in to_remove:
+                del G[k]
 
     elif train_data[-1] == "No":
         for j in range(num_attrs - 1):
-            if train_data[j] != S[j] != "?":
+            if train_data[j] != S[j] and train_data[j] != "?":
                 g = ["?"] * num_attrs
                 g[j] = S[j]
                 G.append(g)
+
+    to_remove = []
+    for k in range(1, len(G)):
+        if G[k] == G_null[0]:
+            to_remove.append(k)
+    for k in to_remove:
+        del G[k]
 
     print("-" * 10)
     print(f"D{i + 1}: {train_data}")
